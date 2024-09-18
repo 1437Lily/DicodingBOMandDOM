f55eaa5717f2dc8c4f80bdd84581470a610cef50
Irfan Hafidh <bamaarmmawadin@gmail.com>
2024-09-18 18:08:29 +0700

    code utuh dari dicoding

--- script.js
+++ script.js
@@ -12,7 +12,28 @@ window.onload = function() {
         }
     }
     const button = document.querySelectorAll('.button');
-
+    const playButton = button[3];
+    const playButtonElement = playButton.children[0];
+    playButtonElement.setAttribute('type','submit');
+    const links = document.getElementById('links');
+    const dicodingLink = document.getElementById('dicodingLink');
+    dicodingLink.onmouseover = function(){
+        dicodingLink.innerText = 'Tempat Belajar Asiky :)';
+    }
+    dicodingLink.onmouseout = function(){
+        dicodingLink.innerText = 'Dicoding'
+    }
+    const googleLink = document.getElementById('googleLink');
+    googleLink.onmouseover = function(){
+        googleLink.innerHTML = '<a href="http://www.google.com" id="googleLink"><b><i>Mysterious</i></b></a>';
+    }
+    googleLink.onmouseout = function(){
+        googleLink.innerHTML = 'Google'
+    };
+    const buttonsi = document.getElementsByClassName('button');
+    for(const a of buttonsi){
+        a.children[0].style.borderRadius = '6px'
+    }
 
 };
 
